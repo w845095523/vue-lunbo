f5f78fcf36822ba3b2c991e74b913e9fa56bab96
w845095523 <55081378+w845095523@users.noreply.github.com>
2019-11-01 19:45:27 +0800

    Add files via upload

---
+++ src/views/vue-lunbo/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="hello">
+        <Carousel :count="2"></Carousel>
+        <Carousel :count="5"></Carousel>
+        <Carousel :count="10"></Carousel>
+    </div>
+</template>
+<script>
+import Carousel from '../components/carousel'
+export default {
+    props:{
+
+    },
+    components:{
+        Carousel:Carousel
+    },
+    data(){
+        return {
+            list:[]
+        }
+    },
+    computed:{
+        
+    },
+    methods:{
+
+    },
+    created(){
+        
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
